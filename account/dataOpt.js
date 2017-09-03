e7f14978a83dc91c9f68415fe47d78a12f394fbc
libraxss <libraxss@gmail.com>
2017-09-04 07:03:13 +0800

    [init] temp commit 20170904

---
+++ account/dataOpt.js
@@ -0,0 +1,63 @@
+const mysql = require("mysql");
+const redis = require("redis");
+const selfConfig = require("./config/db");
+
+const dbConfig = selfConfig.account;
+const redConfig = selfConfig.token;
+
+let db_self = {};
+db_self.isConnected = false;
+
+let redisClient = redis.createClient(redConfig);
+
+module.exports = {
+    connect: () => {
+        db_self = mysql.createConnection(dbConfig);
+        db_self.connect(err => {
+            if (err) {
+                logger.error("[Db] connect error: %s", JSON.stringify(err));
+            } else {
+                logger.info("[Db] connected: %s", this.conf.name);
+                this.isConnected = true;
+            }
+        });
+    },
+    verifyToken: (token, user_name) => {
+        return redisClient.hgetallAsync(token, user_name);
+    },
+    updateToken: (token, info) => {
+        return redisClient.HMSET(token, info);
+    },
+
+    queryUserInfo: (...params) => {
+        let SQL_QUERY_USER_INFO = "SELECT * FROM account WHERE ?? = ? ";
+        return new Promise((resolve, reject) => {
+            db_self.query(SQL_QUERY_USER_INFO, [...params], (err, ret) => {
+                if (err) {
+                    reject(err);
+                } else if (ret) {
+                    resolve(ret);
+                } else {
+                    resolve(null);
+                }
+            });
+        });
+    },
+
+    addUserInfo: (values) => {
+        let SQL_INSERT_USER_INFO = " INSERT INTO account SET ? ";
+        return new Promise((resolve, reject) => {
+            db_self.query(SQL_INSERT_USER_INFO, [values], (err, ret) => {
+                if (err) {
+                    reject(err);
+                } else if (ret.affectedRows) {
+                    resolve(ret);
+                } else {
+                    reject(null);
+                }
+            });
+        });
+    },
+
+
+};
--- account/service.js
+++ account/service.js
@@ -1,14 +1,30 @@
-/* jshint esversion: 6 */
 const Courier = require("node-process-bearer").Courier;
 const logger = require("node-process-bearer").logger.getLogger();
 
 let export_func = {
     name: "account",
-    verify: (...args) => {
-        logger.info(`[account] args: ${[...args]}`);
-        return false;
+    init: () => {
+
+    },
+    asyncRegister: (user_name, code) => {
+
+    },
+    asyncLogin: (user_name, passwd) => {
+
+    },
+    asyncUpdateInfo: (token) => {
+
+    },
+    asyncVerify: (token, module) => {
+
+    },
+
+    asyncAddUser: (user_info) => {
+
     }
 };
 
 let courier = new Courier(export_func);
 courier.listening();
+
+module.exports = export_func;
