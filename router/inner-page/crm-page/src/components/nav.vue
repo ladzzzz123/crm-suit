7d8d478ddb139df909edd8207f8b9c7a0e244f93
songshan.xu <songshan.xu@cootek.cn>
2017-09-26 15:43:19 +0800

    [init] update

---
+++ router/inner-page/crm-page/src/components/nav.vue
@@ -0,0 +1,25 @@
+<template>
+    <nav aria-label="Page navigation">
+        <ul class="pagination">
+            <li v-for="index in indexInfo.indexs" @click="setCurPage(index)" v-bind:key="index">
+                <a v-if="index === indexInfo.curIndex" class="selected">{{ index + 1 }}</a>
+                <a v-else >{{ index + 1 }}</a>
+            </li>
+        </ul>
+    </nav> 
+</template>
+
+<script>
+export default {
+    props: ["indexInfo"],
+    methods: {
+        setCurPage: function(index) {
+            this.$emit("setCurPage", index);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
