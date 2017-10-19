53f1580b55eeaa0e54cc113e473e6b91d6b7dcc4
songshan.xu <songshan.xu@cootek.cn>
2017-10-19 17:52:17 +0800

    [init] update chat

--- chat/service.js
+++ chat/service.js
@@ -1,18 +1,33 @@
-const io = require("socket.io");
+const server = require("http").createServer();
+const io = require('socket.io')(server);
 const logger = require("node-process-bearer").logger.getLogger();
-
 const DEFAULT_CHAT_PORT = 3006;
 
-const chat = io.listen(DEFAULT_CHAT_PORT);
+server.listen(DEFAULT_CHAT_PORT);
 
-chat
-    .of("/crm-chat")
-    .on("connection", socket => {
-        logger.warn("[socket] socket connection!");
-        socket.on("message", msg => {
-            //向所有客户端广播发布的消息
-            logger.warn("socket msg: %s", JSON.stringify(msg));
-            chat.emit("message", msg);
-            // console.log(msg.u_name + '说：' + msg.msg);
-        });
+io.on("connection", function(socket) {
+    socket.emit("message", { msg: "hello world!", u_name: "service" });
+    socket.on("message", msg => {
+        logger.info(JSON.stringify(msg));
+        socket.emit("message", msg);
     });
+});
+
+
+// const io = require("socket.io");
+// const http = require("http");
+
+
+// const chat = io.listen(DEFAULT_CHAT_PORT);
+
+// chat
+//     .of("/crm-chat")
+//     .on("connection", socket => {
+//         logger.warn("[socket] socket connection!");
+//         socket.on("message", msg => {
+//             //向所有客户端广播发布的消息
+//             logger.warn("socket msg: %s", JSON.stringify(msg));
+//             chat.emit("message", msg);
+//             // console.log(msg.u_name + '说：' + msg.msg);
+//         });
+//     });
