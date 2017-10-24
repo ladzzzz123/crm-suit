f7430d8ee49de9c1b71981153b5e58ff47399be2
songshan.xu <songshan.xu@cootek.cn>
2017-10-24 15:55:58 +0800

    [init] update

--- router/crm-neo-page-dist/js/res.js
+++ router/crm-neo-page-dist/js/res.js
@@ -1,1 +1,1 @@
-webpackJsonp([2],{75:function(n,o,p){"use strict";p(76)},76:function(n,o,p){n.exports=p.p+"../img/logo.png"}},[75]);
+webpackJsonp([2],{78:function(n,o,p){"use strict";p(79)},79:function(n,o,p){n.exports=p.p+"../img/logo.png"}},[78]);
