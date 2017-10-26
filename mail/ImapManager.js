00586b86b60cd36c05f75ec5dd959d9d900013bb
songshan.xu <songshan.xu@cootek.cn>
2017-10-26 17:05:45 +0800

    [earnings] add html parse

--- mail/ImapManager.js
+++ mail/ImapManager.js
@@ -91,7 +91,7 @@ class ImapManager {
                     }
                     let m_to = mail.to.value[0].address || "";
                     let m_date = new Date(mail.date) || new Date();
-                    let $ = cheerio.load(mail.html).html();
+                    let $ = cheerio.load(mail.html);
                     let m_content = $("body").html() || "";
                     logger.warn(`[ImapManager] mail m_content:${m_content}`);
 
