d97cc9a75944fbece00f8199763c81513921325f
songshan.xu <songshan.xu@cootek.cn>
2017-10-30 11:55:09 +0800

    [ad-preview] update

--- router/inner-page/crm-neo-page/src/pages/ad-preview.vue
+++ router/inner-page/crm-neo-page/src/pages/ad-preview.vue
@@ -11,15 +11,17 @@
         left: 50%;
         margin-left: -178px;
         position: absolute;
+        text-align: left;
         top: 98px;
         width: 356px;
     }
 
     .openning-img {
-        height: 74%;
+        height: 540px;
         left: 0;
         position: absolute;
         top: 0;
+        width: 100%;
     }
 
     .openning-bg {
@@ -35,14 +37,15 @@
         color: white;
         font-size: 0.08rem;
         left: 0;
-        top: 70%;
+        top: 75%;
         padding: 0.02rem;
         position: absolute;
         width: 0.14rem;
+        -webkit-transform: scale(0.8);
     }
 
     .calling-img {
-        top: 37%;
+        bottom: 37%;
         position: absolute;
         width: 100%;
     }
@@ -61,7 +64,7 @@
         padding: 0.02rem;
         position: absolute;
         width: 0.14rem;
-        -webkit-transform: scale(0.8);
+        -webkit-transform: scale(0.6);
     }
     .calling-close {
         right: 0.1rem;
@@ -71,6 +74,13 @@
     }
 
     .hangup-img {
+        height: 100%;
+        left: 0;
+        position: absolute;
+        top: 0;
+        width: 100%;
+    }
+    .hangup-mask {
         height: 100%;
         left: 0;
         position: absolute;
@@ -96,7 +106,8 @@
                 <div class="content">
                     <div :class="adInfo.pos + '-tips' + ' layer-top' ">广告</div>
                     <img :class="adInfo.pos + '-img' + ' layer-middle' " :src="adInfo.img" />
-                    <img :class="adInfo.pos + '-bg' + ' layer-bottom' " :src="adInfo.bg"/>
+                    <img v-if="adInfo.bg" :class="adInfo.pos + '-bg' + ' layer-bottom' " :src="adInfo.bg"/>
+                    <img v-if="adInfo.mask" :class="adInfo.pos + '-bg' + ' layer-middle' " :src="adInfo.mask"/>
                     <img v-if="adInfo.close" :class="adInfo.pos + '-close' + ' layer-top' " :src="adInfo.close"/>
                 </div>
             </Row>
@@ -130,7 +141,7 @@ export default {
             adImgs: [
                { pos: "openning", title: "开屏",img: "", bg: "img/openning-bg.jpg" },
                { pos: "calling", title: "拨号前", img: "", bg: "img/calling-bg.jpg", close: "http://121.52.235.231:40718/upload_img/static/img/close.png" },
-               { pos: "hangup", title: "挂机",img: "", bg:"img/preview-mask.png" },
+               { pos: "hangup", title: "挂机",img: "", mask:"img/preview-mask.png" },
                { pos: "banner", title: "Banner",img: "" }
             ],
             uploadData:{},
