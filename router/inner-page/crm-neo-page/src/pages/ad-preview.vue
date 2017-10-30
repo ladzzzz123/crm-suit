4480d3777e99f84712ceeeb72ebc4bb7aaea3b8c
songshan.xu <songshan.xu@cootek.cn>
2017-10-30 11:31:53 +0800

    [ad-preview] update

--- router/inner-page/crm-neo-page/src/pages/ad-preview.vue
+++ router/inner-page/crm-neo-page/src/pages/ad-preview.vue
@@ -16,11 +16,8 @@
     }
 
     .content img {
-        height: 100%;
         left: 0;
         position: absolute;
-        top: 0;
-        width: 100%;
     }
 
     .openning-img {
@@ -38,7 +35,7 @@
     }
 
     .openning-tips {
-        background: rgba(155, 170,0, 0.6);
+        background: rgba(55, 55, 55, 0.6);
         border-radius: 3px;
         color: white;
         font-size: 0.12rem;
@@ -49,8 +46,33 @@
         width: 0.14rem;
     }
 
-    .dial-img {
-
+    .calling-img {
+        top: 37%;
+        position: absolute;
+        width: 100%;
+    }
+    .calling-bg {
+        bottom: 0;
+        position: absolute;
+        width: 100%;
+    }
+    .calling-tips {
+        background: rgba(55, 55, 55, 0.8);
+        border-radius: 2px;
+        color: white;
+        font-size: 0.08rem;
+        left: 0;
+        top: 38%;
+        padding: 0.02rem;
+        position: absolute;
+        width: 0.14rem;
+        -webkit-transform: scale(0.8);
+    }
+    .calling-close {
+        right: 0;
+        top: 38%;
+        position: absolute;
+        width: 0.2rem;
     }
 
     .hangup-img {
@@ -72,14 +94,15 @@
     <Row v-if="logged">
         <Card v-for="adInfo in adImgs" v-bind:key="adInfo" class="container">
             <p slot="title">
-                adInfo.title
+                {{ adInfo.title }}
             </p>
             <Row>
                 <img class="bg" src="img/preview-iphone.jpg" />
                 <div class="content">
-                    <div :class="adInfo.pos + '-tips' ">广告</div>
-                    <img :class="adInfo.pos + '-img' " :src="adInfo.img" />
-                    <img :class="adInfo.pos + '-bg' " :src="adInfo.bg"/>
+                    <div :class="adInfo.pos + '-tips' + ' layer-top' ">广告</div>
+                    <img :class="adInfo.pos + '-img' + ' layer-middle' " :src="adInfo.img" />
+                    <img :class="adInfo.pos + '-bg' + ' layer-bottom' " :src="adInfo.bg"/>
+                    <img v-if="adInfo.close" :class="adInfo.pos + '-close' + ' layer-top' " :src="adInfo.close"/>
                 </div>
             </Row>
             <Upload
@@ -111,7 +134,7 @@ export default {
         return { 
             adImgs: [
                { pos: "openning", title: "开屏",img: "", bg: "img/openning-bg.jpg" },
-               { pos: "dial", title: "拨号前", img: "", bg: "" },
+               { pos: "calling", title: "拨号前", img: "", bg: "img/calling-bg.jpg", close: "http://121.52.235.231:40718/upload_img/static/img/close.png" },
                { pos: "hangup", title: "挂机",img: "img/preview-mask.png" },
                { pos: "banner", title: "Banner",img: "" }
             ],
