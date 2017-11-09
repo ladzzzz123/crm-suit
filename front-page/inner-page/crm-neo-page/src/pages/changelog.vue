258fb61a17ea1fbe3abcc80d72065a45183d4f48
songshan.xu <songshan.xu@cootek.cn>
2017-11-09 11:23:28 +0800

    [changelog] add changelog

---
+++ front-page/inner-page/crm-neo-page/src/pages/changelog.vue
@@ -0,0 +1,30 @@
+<template>
+    <Timeline>
+        <TimelineItem v-for="log in changelog" :key="log">
+            <p class="time">{{ log[0] }}</p>
+            <p class="content">{{ log[1] }}</p>
+        </TimelineItem>
+    </Timeline>
+</template>
+
+<script>
+import CHANGELOG_DATA from "../data/changelog.json";
+
+export default {
+    data: () => {
+        return {
+            changelog: CHANGELOG_DATA
+        }
+    }
+}
+</script>
+
+<style>
+    .time{
+        font-size: 14px;
+        font-weight: bold;
+    }
+    .content{
+        padding-left: 5px;
+    }
+</style>
